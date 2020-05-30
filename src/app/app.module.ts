6a7f1d24bd90643a47201ae4f571bdac93d27615
Nhat Quang Ta <nhatquangta2911@gmail.com>
2020-05-30 15:21:16 +0700

    Demo component with type, template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/book.ts
@@ -0,0 +1,7 @@
+export class Book {
+  constructor(
+    public title: string,
+    public price: number,
+    public tags: string[]
+  ) {}
+}
